42b9f104129c3bfdb6955152a9c5f53583bd8c18
theradius <prohexbyte@gmail.com>
2021-10-16 21:58:56 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='noscope',
+    version='1.0.0',
+    author='theradius',
+    keywords='Python scope snapshot tool',
+    packages=['noscope'],
+    description='Python scope snapshot tool',
+    long_description='Tool for saving and loading snapshots of a stack frame',
+    long_description_content_type='text/plain',
+    url='https://github.com/theradius/noscope',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 3'
+    ]
+)
+
